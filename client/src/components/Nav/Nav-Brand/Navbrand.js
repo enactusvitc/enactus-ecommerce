c98312f01fe029a37213fa1e8c483d77a71cd248
Mohammad Shaad <mohammadshaadshaikh@gmail.com>
2023-06-19 22:52:22 +0530

    :fire: Initial commit :tada:

---
+++ client/src/components/Nav/Nav-Brand/Navbrand.js
@@ -0,0 +1,17 @@
+import "./NavBrand.css";
+import { Link } from "react-router-dom";
+import Logo from "../../../asset/img/enactus-logo-gray.png";
+
+const NavBrand = () => {
+  return (
+    <div href="#home" className="navbrand__container">
+      <h1 className="navbrand">
+        <Link to="/">
+          <img src={Logo} alt="Logo" className="navbrand__logo" />
+        </Link>
+      </h1>
+    </div>
+  );
+};
+
+export default NavBrand;
